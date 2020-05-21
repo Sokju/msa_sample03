312c120d2cb59b022bc2945e48130caf6bf3b88a
sjuhwang <sjuhwang@sjuhwang>
2020-05-22 01:18:27 +0900

    20200521

--- platform_config/redeploy.sh
+++ platform_config/redeploy.sh
@@ -1,14 +1,19 @@
-#kubectl delete -f msa3-svc-member/deployment.yml
-#kubectl delete -f msa3-svc-order/deployment.yml
+kubectl delete -f msa3-svc-member/deployment.yml
+kubectl delete -f msa3-svc-order/deployment.yml
+kubectl delete -f msa3-svc-ui/deployment.yml
 
 docker build -t 192.168.100.102:5000/msasmp03/msa3-svc-order:1.0     msa3-svc-order/.
 docker build -t 192.168.100.102:5000/msasmp03/msa3-svc-member:1.0    msa3-svc-member/.
+docker build -t 192.168.100.102:5000/msasmp03/msa3-svc-ui:1.0    msa3-svc-ui/.
 
 docker push 192.168.100.102:5000/msasmp03/msa3-svc-order:1.0
 docker push 192.168.100.102:5000/msasmp03/msa3-svc-member:1.0
+docker push 192.168.100.102:5000/msasmp03/msa3-svc-ui:1.0
 
 kubectl apply -f <(istioctl kube-inject -f msa3-svc-order/deployment.yml)
 kubectl apply -f <(istioctl kube-inject -f msa3-svc-member/deployment.yml)
+kubectl apply -f <(istioctl kube-inject -f msa3-svc-ui/deployment.yml)
 
 kubectl rollout restart deployment/msa3-svc-order
 kubectl rollout restart deployment/msa3-svc-member
+kubectl rollout restart deployment/msa3-svc-ui
